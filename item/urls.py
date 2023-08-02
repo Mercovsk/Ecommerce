fc5ee2a4491bb0d4533155b8053d7f9057a1e5eb
Mercovsk <marcbaja08@gmail.com>
2023-08-02 13:04:22 +0800

    feat: Add endpoint for search API

--- item/urls.py
+++ item/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name ='item'
 
 urlpatterns = [
+    path('', views.items, name='items'),
     path('<int:pk>/', views.detail, name='detail'),
     path('new_item/', views.new_item, name='new_item'),
     path('delete_item/<int:pk>/', views.delete_item, name='delete_item'),
