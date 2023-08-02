5b699ca8697d0620e58a03425c5a7a45fc492071
Mercovsk <marcbaja08@gmail.com>
2023-08-02 12:28:28 +0800

    feat: Create API for edit_item and edit item.detail.html for edit button

--- item/views.py
+++ item/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 
 
@@ -30,6 +30,22 @@ def new_item(request):
     return render(request, 'item/form.html', {
         'form': form,
         'title': "New Item"
+    })
+
+@login_required
+def edit_item(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': "Edit Item"
     })
 
 @login_required
