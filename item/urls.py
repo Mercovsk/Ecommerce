877db81813e36d1e546bd1f36c5e7231a00d8812
Mercovsk <marcbaja08@gmail.com>
2023-08-02 11:54:39 +0800

    feat: Add endpoint for delete_item on item.urls.py

--- item/urls.py
+++ item/urls.py
@@ -7,4 +7,5 @@ app_name ='item'
 urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
     path('new_item/', views.new_item, name='new_item'),
+    path('delete_item/<int:pk>/', views.delete_item, name='delete_item'),
 ]
